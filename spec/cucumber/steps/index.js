dd5872be9a22b23e3dc78c95dcd441c401f7a721
Iulian Coretchi <iulian.coretchi@gmail.com>
2019-03-21 15:12:08 +0200

    WIP Implement Create User with Empty Payload

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,19 +1,34 @@
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 
-When('the client creates a POST request to /users', function(callback) {
-  callback(null, 'pending');
+let request;
+let result;
+let error;
+
+When('the client creates a POST request to /users', function() {
+  request = superagent('POST', 'localhost:8080/users');
 });
 
-When('attaches a generic empty payload', function(callback) {
-  callback(null, 'pending');
+When('attaches a generic empty payload', function() {
+  return undefined;
 });
 
 When('sends the request', function(callback) {
-  callback(null, 'pending');
+  request
+    .then(response => {
+      result = response.res;
+      callback();
+    })
+    .catch(errResponse => {
+      error = errResponse.response;
+      callback();
+    });
 });
 
-Then('our API should respond with a 400 HTTP status code', function(callback) {
-  callback(null, 'pending');
+Then('our API should respond with a 400 HTTP status code', function() {
+  if (error.statusCode !== 400) {
+    throw new Error();
+  }
 });
 
 Then('the payload of the response should be a JSON object', function(callback) {
